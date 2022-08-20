9e7f08a09580bf38c0175552a87d8478c3d97772
Guilherme Sinoara <gsinoara@gmail.com>
2022-08-20 12:23:13 -0300

    added System struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,12 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub struct System {
+    mass: f32,
+    subsystems: Vec<System>,
+    description: String,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl System {
+    pub fn total_mass(&self) -> f32 {
+        unimplemented!();
+        //self.subsystems.iter().fold(self.mass, |total, s| total + s.total_mass())
     }
 }
