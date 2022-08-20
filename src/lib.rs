ae469fda2dbc2e5a6c897820906fa6f0fed4b385
Guilherme Sinoara <gsinoara@gmail.com>
2022-08-20 13:04:21 -0300

    added constructor for System

--- src/lib.rs
+++ src/lib.rs
@@ -9,4 +9,8 @@ impl System {
         unimplemented!();
         //self.subsystems.iter().fold(self.mass, |total, s| total + s.total_mass())
     }
+
+    pub fn new(mass: f32, subsystems: Vec<System>, description: String) -> System {
+        System{mass: mass, subsystems: subsystems, description: description}
+    }
 }
