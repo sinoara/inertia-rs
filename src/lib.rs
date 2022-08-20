a7ca891873bb65d63c5a9761a6468a9c5b06fafc
Guilherme Sinoara <gsinoara@gmail.com>
2022-08-20 19:44:49 -0300

    added position and moment of inertia

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,9 @@
+extern crate nalgebra as na;
+
+
 pub struct System {
     mass: f32,
+    position: [f32; 3],
     moment_of_inertia: [[f32; 3];3],
     subsystems: Vec<System>,
     description: String,
@@ -10,8 +14,13 @@ impl System {
         self.subsystems.iter().fold(self.mass, |total, s| total + s.total_mass())
     }
 
-    pub fn new(mass: f32, moment_of_inertia: [[f32; 3];3], subsystems: Vec<System>, description: String) -> System {
+    pub fn total_inertia(&self) -> [[f32; 3]; 3] {
+        unimplemented!()
+    }
+
+    pub fn new(mass: f32, position: [f32; 3], moment_of_inertia: [[f32; 3];3], subsystems: Vec<System>, description: String) -> System {
         System{mass: mass,
+               position: position,
                moment_of_inertia: moment_of_inertia,
                subsystems: subsystems,
                description: description}
