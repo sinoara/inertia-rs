17e6ae02ae650452bb193cf49d29d07762fff4c8
Guilherme Sinoara <gsinoara@gmail.com>
2022-08-22 18:17:21 -0300

    cargo fmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,4 @@
-use nalgebra::{Vector3, Matrix3};
-
+use nalgebra::{Matrix3, Vector3};
 
 pub struct System {
     mass: f32,
@@ -11,18 +10,28 @@ pub struct System {
 
 impl System {
     pub fn total_mass(&self) -> f32 {
-        self.subsystems.iter().fold(self.mass, |total, s| total + s.total_mass())
+        self.subsystems
+            .iter()
+            .fold(self.mass, |total, s| total + s.total_mass())
     }
 
     pub fn total_inertia(&self) -> Matrix3<f32> {
         unimplemented!()
     }
 
-    pub fn new(mass: f32, position: Vector3<f32>, moment_of_inertia: Matrix3<f32>, subsystems: Vec<System>, description: String) -> System {
-        System{mass: mass,
-               position: position,
-               moment_of_inertia: moment_of_inertia,
-               subsystems: subsystems,
-               description: description}
+    pub fn new(
+        mass: f32,
+        position: Vector3<f32>,
+        moment_of_inertia: Matrix3<f32>,
+        subsystems: Vec<System>,
+        description: String,
+    ) -> System {
+        System {
+            mass: mass,
+            position: position,
+            moment_of_inertia: moment_of_inertia,
+            subsystems: subsystems,
+            description: description,
+        }
     }
 }
--- tests/system.rs
+++ tests/system.rs
@@ -1,6 +1,5 @@
 use inertia_rs::*;
-use nalgebra::{Vector3, Matrix3};
-
+use nalgebra::{Matrix3, Vector3};
 
 #[cfg(test)]
 mod tests {
@@ -8,33 +7,59 @@ mod tests {
 
     #[test]
     fn mass_simple_system() {
-        let system = System::new(1.5, Vector3::from_iterator([0.0;3]), Matrix3::from_iterator([0.0;9]), vec![], String::from("sys"));
+        let system = System::new(
+            1.5,
+            Vector3::from_iterator([0.0; 3]),
+            Matrix3::from_iterator([0.0; 9]),
+            vec![],
+            String::from("sys"),
+        );
 
         assert_eq!(system.total_mass(), 1.5);
     }
 
     #[test]
     fn mass_zero_mass_system() {
-        let system = System::new(0.0, Vector3::from_iterator([0.0;3]), Matrix3::from_iterator([0.0;9]), vec![], String::from("sys"));
+        let system = System::new(
+            0.0,
+            Vector3::from_iterator([0.0; 3]),
+            Matrix3::from_iterator([0.0; 9]),
+            vec![],
+            String::from("sys"),
+        );
 
         assert_eq!(system.total_mass(), 0.0);
     }
 
     #[test]
     fn mass_composed_system() {
-        let sub_system = System::new(2.4, Vector3::from_iterator([0.0;3]), Matrix3::from_iterator([0.0;9]), vec![], String::from("sys"));
-        let system = System::new(1.7, Vector3::from_iterator([0.0;3]), Matrix3::from_iterator([0.0;9]), vec![sub_system], String::from("sys"));
+        let sub_system = System::new(
+            2.4,
+            Vector3::from_iterator([0.0; 3]),
+            Matrix3::from_iterator([0.0; 9]),
+            vec![],
+            String::from("sys"),
+        );
+        let system = System::new(
+            1.7,
+            Vector3::from_iterator([0.0; 3]),
+            Matrix3::from_iterator([0.0; 9]),
+            vec![sub_system],
+            String::from("sys"),
+        );
 
-        assert_eq!(system.total_mass(), 2.4+1.7);
+        assert_eq!(system.total_mass(), 2.4 + 1.7);
     }
 
     #[test]
     fn inertia_simple_system() {
         let system = System::new(
             1.5,
-            Vector3::from_iterator([0.0;3]),
+            Vector3::from_iterator([0.0; 3]),
             Matrix3::identity(),
-            vec![], String::from("sys"));
+            vec![],
+            String::from("sys"),
+        );
 
         assert_eq!(system.total_inertia(), Matrix3::identity());
     }
@@ -42,28 +67,66 @@ mod tests {
     #[test]
     fn inertia_same_center_system() {
         let sub_system = System::new(
-            1.5, Vector3::from_iterator([0.0;3]),
+            1.5,
+            Vector3::from_iterator([0.0; 3]),
             Matrix3::from_iterator([1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0]),
-            vec![], String::from("sys"));
+            vec![],
+            String::from("sys"),
+        );
         let system = System::new(
-            1.5, Vector3::from_iterator([0.0;3]),
+            1.5,
+            Vector3::from_iterator([0.0; 3]),
             Matrix3::from_iterator([1.5, 0.0, 0.0, 0.0, 0.9, 0.0, 0.0, 0.0, 1.1]),
-            vec![sub_system], String::from("sys"));
+            vec![sub_system],
+            String::from("sys"),
+        );
 
-        assert_eq!(system.total_inertia(), Matrix3::from_iterator([1.0+1.5, 0.0, 0.0, 0.0, 1.0+0.9, 0.0, 0.0, 0.0, 1.0+1.1]));
+        assert_eq!(
+            system.total_inertia(),
+            Matrix3::from_iterator([
+                1.0 + 1.5,
+                0.0,
+                0.0,
+                0.0,
+                1.0 + 0.9,
+                0.0,
+                0.0,
+                0.0,
+                1.0 + 1.1
+            ])
+        );
     }
 
     #[test]
     fn inertia_subsystem() {
         let sub_system = System::new(
-            2.0, Vector3::from_iterator([3.0, 0.0, 0.0]),
+            2.0,
+            Vector3::from_iterator([3.0, 0.0, 0.0]),
             Matrix3::zeros(),
-            vec![], String::from("sys"));
+            vec![],
+            String::from("sys"),
+        );
         let system = System::new(
-            0.0, Vector3::from_iterator([0.0;3]),
+            0.0,
+            Vector3::from_iterator([0.0; 3]),
             Matrix3::zeros(),
-            vec![sub_system], String::from("sys"));
+            vec![sub_system],
+            String::from("sys"),
+        );
 
-        assert_eq!(system.total_inertia(), Matrix3::from_iterator([0.0, 0.0, 2.0*3.0*3.0, 0.0, 0.0, 0.0, 0.0, 0.0, 2.0*3.0*3.0]));
+        assert_eq!(
+            system.total_inertia(),
+            Matrix3::from_iterator([
+                0.0,
+                0.0,
+                2.0 * 3.0 * 3.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                2.0 * 3.0 * 3.0
+            ])
+        );
     }
 }
