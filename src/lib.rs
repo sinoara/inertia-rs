71ffec1aa86af0e7a24463ed2c7a68bcc091933a
Guilherme Sinoara <gsinoara@gmail.com>
2022-09-07 16:31:05 -0300

    change subsystems to Inertia trait and added needed getters

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ pub struct System {
     mass: f32,
     position: Vector3<f32>,
     moment_of_inertia: Matrix3<f32>,
-    subsystems: Vec<System>,
+    subsystems: Vec<Box<dyn Inertia>>,
     description: String,
 }
 
@@ -12,6 +12,9 @@ pub trait Inertia {
     fn total_mass(&self) -> f32;
     fn total_inertia(&self) -> Matrix3<f32>;
     fn center_of_mass(&self) -> Vector3<f32>;
+
+    fn get_mass(&self) -> f32;
+    fn get_position(&self) -> Vector3<f32>;
 }
 
 impl Inertia for System {
@@ -27,17 +30,25 @@ impl Inertia for System {
             .fold(self.moment_of_inertia, |total, s| {
                 total
                     + s.total_inertia()
-                    + s.mass
-                        * (s.position.dot(&s.position) * Matrix3::identity()
-                            - s.position * s.position.transpose())
+                    + s.get_mass()
+                        * (s.get_position().dot(&s.get_position()) * Matrix3::identity()
+                            - s.get_position() * s.get_position().transpose())
             })
     }
 
     fn center_of_mass(&self) -> Vector3<f32> {
         self.subsystems
             .iter()
-            .fold(Vector3::zeros(), |total, s| total + s.total_mass()*s.position)
+            .fold(Vector3::zeros(), |total, s| total + s.total_mass()*s.get_position())
             /self.total_mass()
+    }
+
+    fn get_mass(&self) -> f32 {
+        self.mass
+    }
+
+    fn get_position(&self) -> Vector3<f32> {
+        self.position
     }
 }
 
@@ -46,7 +57,7 @@ impl System {
         mass: f32,
         position: Vector3<f32>,
         moment_of_inertia: Matrix3<f32>,
-        subsystems: Vec<System>,
+        subsystems: Vec<Box<dyn Inertia>>,
         description: String,
     ) -> System {
         System {
